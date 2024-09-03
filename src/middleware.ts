cccfd94fd6789533cfc16a85d28f5be5b04e5d4f
JuanBaeza <juanbaezabaeza@outlook.com>
2024-09-02 21:23:41 -0400

    feat: Commented out middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
 import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-  const { request, redirect } = context;
-  const visitedCookie = 'visited=true';
-  const redirectPath = '/welcome';
-  const rootPath = '/';
+  // const { request, redirect } = context;
+  // const visitedCookie = 'visited=true';
+  // const redirectPath = '/welcome';
+  // const rootPath = '/';
 
-  const visited = request.headers.get('cookie')?.includes(visitedCookie);
-  const { pathname } = new URL(request.url);
+  // const visited = request.headers.get('cookie')?.includes(visitedCookie);
+  // const { pathname } = new URL(request.url);
 
-  if (!visited && pathname === rootPath) {
-    return redirect(redirectPath, 302);
-  }
+  // if (!visited && pathname === rootPath) {
+  //   return redirect(redirectPath, 302);
+  // }
 
   return next();
 });
